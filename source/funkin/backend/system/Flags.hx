18767c324eadade9267206e535ad8290ae3ae1e3
kitcliffofc1-ship-it <kitcliff@github.com>
2026-07-10 00:53:03 +0700

    fix: update REPO_OWNER/REPO_NAME to RnK fork; bump version to 1.0.1

--- source/funkin/backend/system/Flags.hx
+++ source/funkin/backend/system/Flags.hx
@@ -50,10 +50,10 @@ class Flags {
 	@:lazy public static var TITLE:String = Application.current.meta.get('name');
 	@:lazy public static var VERSION:String = Application.current.meta.get('version');
 
-	@:lazy public static var VERSION_MESSAGE:String = 'Codename Engine v$VERSION';
+	@:lazy public static var VERSION_MESSAGE:String = 'RnK Engine v$VERSION';
 
-	public static var REPO_NAME:String = "CodenameEngine";
-	public static var REPO_OWNER:String = "CodenameCrew";
+	public static var REPO_NAME:String = "RnK-FNF";
+	public static var REPO_OWNER:String = "kitcliffofc1-ship-it";
 	public static var REPO_URL:String = 'https://github.com/$REPO_OWNER/$REPO_NAME';
 
 	/**
